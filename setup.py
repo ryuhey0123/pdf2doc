1487945076bf9e765e59fbab814e6831d5308593
ryuhey_biz <none>
2020-11-10 20:25:24 +0900

    Pyinstaller delete

--- setup.py
+++ setup.py
@@ -10,17 +10,27 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
 
 
+required = [
+    "pypdf2",
+    "reportlab",
+    "click",
+    "crayons",
+    "send2trash",
+]
+
 setup(
     name="pdf2doc",
-    version="1.1",
+    version="__version__",
     description="Make paged document by PDFs.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="Ryuhei Fujita",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     entry_points={
         "console_scripts": [
             "pdf2doc = pdf2doc:cli",
         ]
     },
+    install_requires=required,
+    license="MIT"
 )
